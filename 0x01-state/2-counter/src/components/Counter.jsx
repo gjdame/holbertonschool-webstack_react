df31e6b4928045479ee07bcb6db8bf3e6ce7bc23
gjdame <gregoryjdame@gmail.com>
2019-07-28 21:01:15 -0700

    add 2-counter to use state instead of props

---
+++ 0x01-state/2-counter/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 98};
+  }
+  render() {
+    return <div>{this.state.value}</div>;
+  }
+}
+
+Counter.PropTypes = {
+  value: PropTypes.number
+};
