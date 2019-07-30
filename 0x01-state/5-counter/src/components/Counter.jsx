ffced084d6c7da371b951ba61701ffced5a8e297
gjdame <gregoryjdame@gmail.com>
2019-07-29 22:09:58 -0700

    add 5-counter

---
+++ 0x01-state/5-counter/src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: this.props.initValue || 0};
+    this.handleclick = this.handleclick.bind(this);
+  }
+
+  // handles click to set value to 1000 from initValue or null
+  handleclick() {
+    this.setState(state => ({
+      value: (state.value + 1)
+    }));
+  }
+  render() {
+    return (
+      <span>
+        <div>{this.state.value}</div>
+        <button onClick={this.handleclick}>New value</button>
+      </span>
+    );
+  }
+}
+
+Counter.PropTypes = {
+  value: PropTypes.number
+};
